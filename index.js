cac2ffbe8eff03b612aa8782a9666c9e94ad0bf3
Mariana <mariana.amfr@gmail.com>
2022-02-20 15:21:26 +0000

    Updated with mongoose logic

--- index.js
+++ index.js
@@ -1,219 +1,202 @@
-// adding express and morgan modules to the project
+// adding modules to the project
 const express = require('express'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
-      uuid = require('uuid');
+      uuid = require('uuid'),
+      mongoose = require('mongoose'),
+      Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // make sure the logging happens before anything else
 app.use(morgan('common'));
 
 // default textual response for endpoint '/'
 app.get('/', (req, res) => {
-  res.send('Welcome to my movie API!');
+  res.send('Welcome to MyFlix!');
 });
 
-let movies = [
-  {
-    title: 'Carol',
-    director: {
-      name: 'Todd Haynes',
-      bio: 'Todd Haynes is an American filmmaker from Los Angeles, California. His films span four decades with consistent themes examining the personalities of well-known musicians, dysfunctional and dystopian societies, and blurred gender roles.',
-      birthYear: 1961,
-      deathYear: null
-    },
-    description: 'Set in New York City during the early 1950s, Carol tells the story of a forbidden affair between an aspiring female photographer and an older woman going through a difficult divorce.',
-    genre: {
-      name: 'Drama',
-      description: 'The drama genre is defined by conflict and often looks to reality rather than sensationalism. Emotions and intense situations are the focus, but where other genres might use unique or exciting moments to create a feeling, movies in the drama genre focus on common occurrences. Drama is a very broad category and untethered to any era — from movies based on Shakespeare to contemporary narratives.'
-    },
-    imageURL: 'https://www.vintagemovieposters.co.uk/wp-content/uploads/2015/12/IMG_0913.jpg',
-    featured: true
-  },
-
-  {
-    title: 'Popstar: Never Stop Never Stopping',
-    director: {
-      name: 'Akiva Schaffer',
-      bio: 'Akiva D. Schaffer is an American film director, comedian, actor, and writer. He is a member of the comedy group The Lonely Island along with Andy Samberg and Jorma Taccone. He began his career with The Lonely Island making videos for Channel 101. In 2005, Saturday Night Live hired the trio, Schaffer joining as a writer.',
-      birthYear: 1977,
-      deathYear: null
-    },
-    description: 'When it becomes clear that his solo album is a failure, a former boy band member does everything in his power to maintain his celebrity status.',
-    genre: {
-      name: 'Comedy',
-      description: 'The comedy genre is defined by events that are intended to make someone laugh, no matter if the story is macabre, droll, or zany. Comedy can be found in most movies, but if the majority of the film is intended to be a comedy you may safely place it in this genre. The best comedy movies range throughout this entire spectrum of humor.'
-    },
-    imageURL: 'https://www.filmstories.co.uk/wp-content/uploads/2021/05/popstar-never-stop.jpg',
-    featured: false
-
-  },
-
-  {
-    title: 'Moulin Rouge',
-    director: {
-      name: 'Baz Lurhman',
-      bio: 'Baz Luhrmann is an Australian writer, director, and producer with projects spanning film, television, opera, theatre, music, and recording industries. He is regarded by many as a contemporary example of an auteur for his distinctly recognizable style and deep involvement in the writing, directing, design, and musical components of all his work. As a storyteller, he\'s known as a pioneer of pop culture, fusing high and low culture with a unique sonic and cinematic language. ',
-      birthYear: 1962,
-      deathYear: null
-    },
-    description: 'A poor Bohemian poet in 1890s Paris falls for a beautiful courtesan and nightclub star whom a jealous duke covets.',
-    genre:
-    { name: 'Musical',
-      description: 'Musicals originated as stage plays, but they soon became a favorite for many film directors and have even made their way into television. Musicals can incorporate any other genre, but they incorporate characters who sing songs and perform dance numbers.'
-  },
-  imageURL: 'https://movieposters.ie/wp-content/uploads/2013/01/MQUAD_MR_.jpg',
-  featured: false
-}
-]
-
-let users = [
-  {
-    id: 1,
-    name: 'Ana Maria',
-    favoriteMovies: []
-  },
-  {
-    id: 2,
-    name: 'Zé Luis',
-    favoriteMovies: ['Carol']
-  }
-]
 
 // get info on all movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// get info on all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+        res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // get info on one movie by title
-app.get('/movies/:title', (req, res) => {
-  const movie = movies.find((movie) => {
-    return movie.title === req.params.title
-  })
-
-  if (movie.title) {
-    res.status(200).json(movie);
-  }
-
-  else {
-    const message ='Sorry, we couldn\'t find this movie.';
-    res.status(400).send(message)
-  }
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne( { Title: req.params.Title })
+    .then((movie) => {
+      res.status(201).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    })
 });
 
 // get info on genre
+// first it finds the movie then you request the info you need from movie entry
 app.get('/movies/genres/:genreName', (req,res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.genre.name === genreName).genre;
-
-  if(genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this genre.')
-  }
+  Movies.findOne( { "Genre.Name": req.params.genreName })
+    .then((movie) => {
+      res.status(201).json(movie.Genre)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    })
 });
 
 // get info on director
 app.get('/movies/directors/:directorName', (req,res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.director.name === directorName).director;
-
-  if(director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this director.')
-  }
+  Movies.findOne( { "Director.Name": req.params.directorName })
+    .then((movie) => {
+      res.status(201).json(movie.Director)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    })
 });
 
 // get films by director name
 app.get('/movies/directors/:directorName/movielist/', (req,res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.director.name === directorName);
-
-  if(director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this director.')
-  }
+      Movies.find( { "Director.Name": req.params.directorName })
+      .then((movies) => {
+        res.status(201).json(movies)
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      })
 });
 
-// add a new user to the database
+//add a new user to the database
 app.post('/users', (req,res) => {
-  const newUser = req.body;
-
-  if(newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  } else {
-    const message ='Missing name in request body.';
-    res.status(400).send(message);
-  }
+  Users.findOne({ Username: req.params.Username })
+    .then ((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => {res.status(201).json(user)})
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-// allow users to update their username
-app.put('/users/:id', (req,res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this user.')
-  }
+// allow users to update their info
+app.put('/users/:Username', (req,res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $set:
+      {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      },
+    },
+  { new: true},
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 // add movie to favorites
-app.post('/users/:id/:movieTitle', (req,res) => {
-  const { id } = req.params;
-  const { movieTitle } = req.params;
-
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(movieTitle + ' has been added to ' + user.name + '\'s list.');
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this user.')
+app.post('/users/:Username/movies/:MovieID', (req,res) => {
+  Users.findOneAndUpdate ({ Username: req.params.Username
+  }, {
+    $push: { FavoriteMovies: req.params.MovieID}
+  },
+  { new: true},
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
   }
+});
 });
 
 // delete movie from favorites
-app.delete('/users/:id/:movieTitle', (req,res) => {
-  const { id } = req.params;
-  const { movieTitle } = req.params;
-
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(movieTitle + ' has been removed from ' + user.name + '\'s list.');
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this user.')
-  }
+app.delete('/users/:Username/movies/:MovieID', (req,res) => {
+  Users.findOneAndUpdate ({ Username: req.params.Username },
+    { $pull: { FavoriteMovies: req.params.MovieID}
+  },
+  { new: true },
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 
 // delete user
-app.delete('/users/:id', (req,res) => {
-  const { id } = req.params;
-
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    users = users.filter(user => user.id !== id)
-    res.status(200).send('User ID ' + id + ' has been deleted.');
-  } else {
-    res.status(400).send('Sorry, we couldn\'t find this user.')
-  }
+app.delete('/users/:Username', (req,res) => {
+  Users.findOneAndRemove( { Username: req.params.Username})
+    .then ((user) => {
+      if(!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
-
 
 app.use(express.static('public'));
 
