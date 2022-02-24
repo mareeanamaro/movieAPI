47f883294d44affe2bcc8fd27212019c4a057998
Mariana <mariana.amfr@gmail.com>
2022-02-24 21:03:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -268,6 +268,7 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false}), (req,
     });
 
     //listener
-    app.listen(8080, () => {
-      console.log('Your app is listening on port 8080.');
-    });
+    const port = process.env.PORT || 8080;
+  app.listen(port, '0.0.0.0',() => {
+   console.log('Listening on Port ' + port);
+});
